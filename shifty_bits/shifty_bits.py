2b5e5085c33a9e8d699c5979075bb4f92da8e1d8
cheetahspots01 <112513827+752986@users.noreply.github.com>
2023-02-05 19:55:20 -0700

    aaaAAAA

--- shifty_bits/shifty_bits.py
+++ shifty_bits/shifty_bits.py
@@ -7,14 +7,18 @@ SEPARATOR = " "
 def get_bits(num: int, start: int, end: int) -> int:
     '''Get the specific bits of a number. `start` and `end` are offsets from the right.'''
 
-    assert end > start
+    assert end >= start # POTENTIAL ERROR: if this is false, then something has gone wrong
 
     length = end - start
     mask = ((1 << length) - 1)
 
-    return (num >> start) & mask
+    return (num >> start) & mask # bitshift to get rid of right bits, then mask to get rid of left bits
 
 def get_signed_bits(num: int, start: int, end: int) -> int:
+    '''Get the specific bits of a number, and treat it as a two's compliment signed int. `start` and `end` are offsets from the right.'''
+
+    assert end > start # >= might work here, but I'm not sure # POTENTIAL ERROR: if this is false, then something has gone wrong
+
     result = get_bits(num, start, end - 1)
 
     negative = (num >> (end - 1)) & 1
@@ -23,13 +27,30 @@ def get_signed_bits(num: int, start: int, end: int) -> int:
     return result + (-sign_weight * negative)
 
 def format_float(num: float) -> str:
-    if num > 99999.99999 or num < 0.00001:
+    if abs(num) > 99999.99999 or abs(num) < 0.00001:
         result = f"{num:.5e}".split("+")
         if len(result[1]) < 3:
-            result[1] = "+0" + result[1]
+            result[1] = "0" + result[1]
+        result[1] = "+" + result[1]
         return result[0] + result[1]
     else:
         return f"{num:.5f}"
+
+# version that strips trailing zeroes
+# def format_float(num: float) -> str:
+#     if abs(num) > 99999.99999 or abs(num) < 0.00001:
+#         result = f"{num:.5e}".split("+")
+#         if len(result[1]) < 3:
+#             result[1] = "0" + result[1]
+#         result[1] = "+" + result[1]
+#         if "." in result[0]:
+#             result[0] = result[0].rstrip("0")
+#         return result[0] + result[1]
+#     else:
+#         result = f"{num:.5f}"
+#         if "." in result:
+#             result = result.rstrip("0")
+#         return result
 
 cases = int(sys.stdin.readline().rstrip())
 
@@ -55,9 +76,9 @@ for case_num in range(cases):
             print(num)
         elif args[0] == "float":
             bits = get_bits(data, start, end)
-            num = struct.unpack("f", struct.pack("I", bits))[0]
+            num = struct.unpack("f", struct.pack("I", bits))[0] # POTENTIAL ERROR: if the wrong number of bits is given, but that shouldn't happen
             print(format_float(num))
         elif args[0] == "double":
             bits = get_bits(data, start, end)
-            num = struct.unpack("d", struct.pack("Q", bits))[0]
+            num = struct.unpack("d", struct.pack("Q", bits))[0] # POTENTIAL ERROR: if the wrong number of bits is given, but that shouldn't happen
             print(format_float(num))
